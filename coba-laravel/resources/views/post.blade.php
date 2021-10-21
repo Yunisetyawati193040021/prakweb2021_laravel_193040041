4efdc01e0e739866d2a006f630bb768ed93a98d2
ViqriFebriana <muhammadviqrifebriana@gmail.com>
2021-10-21 14:36:40 +0700

    menyelesaikan vidio database seeder

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -4,10 +4,10 @@
 
 <h2>{{ $post->title }}</h2>
 
-<p>by. Muhammad Viqri Febriana in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+<p>by. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
 {!! $post->body !!}
 
 
-<a href="/post">Back To Posts</a>
+<a href="/post" class="d-block mt-3">Back To Posts</a>
 @endsection
