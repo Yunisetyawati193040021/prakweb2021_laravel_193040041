844a7a7297d11c2fbea71ef0facc2d2fcc0a0541
ViqriFebriana <muhammadviqrifebriana@gmail.com>
2021-10-28 15:01:28 +0700

    menyelesaikan vidio User Registration

--- coba-laravel/app/Http/Controllers/RegisterController.php
+++ coba-laravel/app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,5 +14,23 @@ class RegisterController extends Controller
             'title' => 'register',
             'active' => 'register'
         ]);
+    }
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        // $validateData['password'] = bcrypt($validateData['password']);
+        $validateData['password'] = Hash::make($validateData['password']);
+
+        User::create($validateData);
+
+        // $request->session()->flash('success', 'Registration successfull! Please Login');
+
+        return redirect('/login')->with('success', 'Registration successfull! Please Login');
     }
 }
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -55,3 +55,5 @@ route::get('/categories', function () {
 Route::get('/login', [LoginController::class, 'index']);
 
 Route::get('/register', [RegisterController::class, 'index']);
+
+Route::post('/register', [RegisterController::class, 'store']);
